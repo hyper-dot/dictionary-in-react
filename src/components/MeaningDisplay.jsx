e37afabe272de4e400cb51742775e7a8f49a2d30
Roshan Paudel <rozanpoudel@gmail.com>
2023-08-17 17:31:40 +0545

    get data from context instead in the component

--- src/components/MeaningDisplay.jsx
+++ src/components/MeaningDisplay.jsx
@@ -7,26 +7,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 const MeaningDisplay = () => {
-  const { data } = useContext(MeaningContext);
-
-  // Extract Parts of Speech
-  const partsOfSpeech = data[0].meanings.map((item) => item.partOfSpeech);
-
-  // Select parts of speech
-  const [selectedPartOfSpeech, setSelectedPartOfSpeech] = useState(
-    partsOfSpeech[0],
-  );
-
-  // All parts of speech
-  const [allPartsOfSpeech, setAllPartsOfSpeech] = useState(partsOfSpeech);
-
-  // Get meanings from selected parts of speech
-  const meanings = data[0].meanings.filter(
-    (item) => (item.partOfSpeech = selectedPartOfSpeech),
-  );
-
-  // meanings to display
-  const [displayMeaning, setDisplayMeaning] = useState(meanings[0]);
+  const { data, displayMeaning, allPartsOfSpeech } = useContext(MeaningContext);
 
   return (
     <>
@@ -43,7 +24,7 @@ const MeaningDisplay = () => {
         ))}
       </div>
       <ol className='list-decimal px-5'>
-        {data
+        {displayMeaning
           ? displayMeaning.definitions.map((d, i) => (
               <li key={i} className={`px-2 ${inter.className}`}>
                 {d.definition}
--- src/context/MeaningsContext.jsx
+++ src/context/MeaningsContext.jsx
@@ -1,15 +1,36 @@
 'use client';
-import { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const MeaningContext = createContext();
 
 export const MeaningProvider = ({ children }) => {
-  const [data, setData] = useState();
+  const [data, setData] = useState(null); // Initialize data as null
+
+  const [selectedPartOfSpeech, setSelectedPartOfSpeech] = useState(null); // Initialize as null
+  const [allPartsOfSpeech, setAllPartsOfSpeech] = useState([]);
+  const [displayMeaning, setDisplayMeaning] = useState(null);
+
+  useEffect(() => {
+    if (data) {
+      // Extract Parts of Speech
+      const partsOfSpeech = data[0].meanings.map((item) => item.partOfSpeech);
+
+      setSelectedPartOfSpeech(partsOfSpeech[0]);
+      setAllPartsOfSpeech(partsOfSpeech);
+
+      // Get meanings from selected parts of speech
+      const meanings = data[0].meanings.filter(
+        (item) => item.partOfSpeech === selectedPartOfSpeech,
+      );
+
+      setDisplayMeaning(meanings[0]);
+    }
+  }, [data, selectedPartOfSpeech]);
 
   function handleSearch(e) {
     e.preventDefault();
     const query = e.target[0].value;
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${query}`) // Replace with your API endpoint
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${query}`)
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => {
@@ -22,6 +43,8 @@ export const MeaningProvider = ({ children }) => {
       value={{
         handleSearch,
         data,
+        allPartsOfSpeech,
+        displayMeaning,
       }}
     >
       {children}
